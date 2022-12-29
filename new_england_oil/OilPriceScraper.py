b37db43e3c2713bbe71ca23d0261ff7c7d27825f
Anthony Murabito <anthony.murabito@gmail.com>
2022-12-29 08:04:56 -0500

    small class updates

--- new_england_oil/OilPriceScraper.py
+++ new_england_oil/OilPriceScraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Parse and Plot Data from New England Oil
+Scrape oil price data from newenglandoil.com
 """
 
 from bs4 import BeautifulSoup as bs
@@ -16,19 +16,19 @@ class OilPriceScraper(object):
         self.zones = {}
         self.URL = ''
         self.url_dict = {}
-
         self.zones['newhampshire'] = [2,6]
         self.zones['massachusetts'] = [8,9]
         self.URL = 'https://www.newenglandoil.com/'
-        self.generate_urls()
+        self._generate_urls()
 
-    def generate_urls(self):
+    def _generate_urls(self):
+        ''' populate a dictionary with the urls of interest '''
         for state in self.zones:
             for zone in self.zones[state]:
                 self.url_dict["{}{}/zone{}.asp?x=0".format(self.URL, state, zone)] = {'state': state, 'zone': zone}
 
 
-    def generate_oil_data(self):
+    def _generate_oil_html(self):
         """Scrape data for each defined zone, and be able to pass that object on"""
         for url in self.url_dict:
             sauce = requests.get(url)
@@ -39,7 +39,16 @@ class OilPriceScraper(object):
         return self.url_dict
 
 
-if __name__ == '__main__':
+    def _generate_oil_dict(self, html_data):
+        ''' Take the html data generated from bs4 and parse it into a dictionary '''
+        return {}
+
+
+def get_oil_data(self):
     ops = OilPriceScraper()
-    data = ops.generate_oil_data()
-    
+    html_data = ops._generate_oil_html()
+    return ops._generate_oil_data(html_data)
+
+
+if __name__ == '__main__':
+
